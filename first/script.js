3765a47045f2a49dc3be38e87cc9a780309632cc
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-18 23:36:21 +0700

    update

--- first/script.js
+++ first/script.js
@@ -6,3 +6,14 @@ console.log(40 + 3 - 19 + 43);
 
 let firstName = "firyal";
 console.log(firstName);
+
+// Variable name conventions
+let firyal_huwaida = "FH";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+let job1 = "programmer";
+let job2 = "teacher";
+console.log(myFirstJob);
