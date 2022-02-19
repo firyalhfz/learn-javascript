bac014cae9d830d722efda32640de4af4a1c358a
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-19 15:31:56 +0700

    learn variable

---
+++ first/assignment.js
@@ -0,0 +1,7 @@
+let country = "Indonesia"
+let continent = "between the continents of Asia and Oceania"
+let population = 278207416
+
+console.log(country)
+console.log(continent)
+console.log(population)
--- first/script.js
+++ first/script.js
@@ -1,19 +1,22 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
 
-40 + 3 - 19 + 43;
+// 40 + 3 - 19 + 43;
 console.log(40 + 3 - 19 + 43);
 
 let firstName = "firyal";
 console.log(firstName);
 
+// variable cannot start with number, symbol (except for $ and _), UpperCase
+// Variables are containers for storing data (storing data values).
+
 // Variable name conventions
 let firyal_huwaida = "FH";
 let $function = 27;
-let person = "jonas";
+let person = "firyal";
 let PI = 3.1415;
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
-let job1 = "programmer";
-let job2 = "teacher";
+let myFirstJob = "Teacher";
+let myCurrentJob = "Coder";
+let job1 = "teacher";
+let job2 = "programmer";
 console.log(myFirstJob);
