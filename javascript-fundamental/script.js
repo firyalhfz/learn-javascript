fded7364aaf75d8d8d9009f9f90943e733c06097
Firyal <97155805+firyalhuwaida@users.noreply.github.com>
2022-02-21 17:02:37 +0700

    add math operations

--- javascript-fundamental/script.js
+++ javascript-fundamental/script.js
@@ -36,3 +36,29 @@ console.log(typeof year);
 year = 1991;
 console.log(typeof year);
 console.log(typeof null);
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageFiryal = now - 1991;
+const ageIlyas = now - 2018;
+console.log(ageFiryal, ageIlyas);
+console.log(ageFiryal * 2, ageFiryal / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+const firstName = 'Firyal';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+// Comparison operators
+console.log(ageFiryal > ageIlyas); // >, <, >=, <=
+console.log(ageIlyas >= 18);
+const isFullAge = ageIlyas >= 18;
+console.log(now - 1991 > now - 2018);
+
