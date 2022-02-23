735659c6b72c4d7d5f486de7e94b1f88869674d4
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-23 13:26:12 +0700

    add : coding challenges about operator

--- javascript-fundamental/assignment.js
+++ javascript-fundamental/assignment.js
@@ -5,3 +5,33 @@ let population = 278207416
 console.log(country)
 console.log(continent)
 console.log(population)
+
+/*coding challenges 1*/
+
+//data 1
+var weightMark = 78
+var weightJohn = 92
+var heightMark = 1.69
+var heightJohn = 1.95
+
+var BMIMark = weightMark / heightMark ** 2
+var BMIJohn = weightJohn / heightJohn ** 2
+var markHigherBMI = BMIMark > BMIJohn
+
+console.log(BMIMark)
+console.log(BMIJohn)
+console.log(markHigherBMI)
+
+//data 2
+var weightMark = 95
+var weightJohn = 85
+var heightMark = 1.88
+var heightJohn = 1.76
+
+var BMIMark = weightMark / heightMark ** 2
+var BMIJohn = weightJohn / heightJohn ** 2
+var markHigherBMI = BMIMark > BMIJohn
+
+console.log(BMIMark)
+console.log(BMIJohn)
+console.log(markHigherBMI)
