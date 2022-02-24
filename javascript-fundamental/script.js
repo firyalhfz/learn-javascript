4eed78adebadca38cb8f6daf23002a89a005755b
firyalhuwaida <iyaalfiryal@gmail.com>
2022-02-24 12:59:45 +0700

    add: Equality Operato: == vs ===

--- javascript-fundamental/script.js
+++ javascript-fundamental/script.js
@@ -135,6 +135,7 @@ multiple
 lines`) 
 */
 
+/*
 //Taking decesions: if else Statement
 const age = 15;
 
@@ -154,6 +155,7 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
 
 // var x = 5;
 // var y = 6;
@@ -172,3 +174,26 @@ console.log(century);
 
 // var name = "Firyal"
 // name = "Fauzi"
+
+//equality Operators: == vs ===
+const age = '18'
+if(age === 18) console.log(`You just became an adult (strict)`)
+if(age == 18) console.log(`You just become an adult(loose)`)
+
+const favourite = Number(prompt(`What's your favourite numbr?`))
+console.log(favourite)
+console.log(typeof favourite)
+
+if(favourite === 23) {
+  console.log(`Cool 23 is amazing number`)
+} else if(favourite === 7){
+  console.log(`7 is also a cool number`)
+} else if(favourite === 9){
+  console.log(`9 is also a cool number`)
+} else {
+  console.log(`Number is not 23 or 7 or 9`)
+}
+
+if(favourite !== 23) console.log(`Why not 23?`)
+
+
