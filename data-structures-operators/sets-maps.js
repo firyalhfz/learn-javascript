5c0b01e602b8aae5ac15b61ff5f28fa1665fa116
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-16 13:14:01 +0700

    update: todo 16 comment to set

--- data-structures-operators/sets-maps.js
+++ data-structures-operators/sets-maps.js
@@ -17,12 +17,25 @@ const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Water'];
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique); // (4) ['Waiter', 'Chef', 'Manager', 'Water']
 
-console.log(
-    new Set(staff).size 
-); // 4
+console.log(new Set(staff).size); // 4
 
-//in sets there are actually no indexes. And in fact, there is no way of getting values out of a set. we cannot call set one by one, but we can use array to call it [...new Set(staff)] like this. 
+/**
+ * the syntax of Set: const variable_name = new Set(['value', 'value'])
+ * to check the length, use size: variable_name.size
+ * to check if the value is exist, use has method: variable_name.has(value)
+ * to add the value to set, use add: variable_name.add(value)
+ * to delete the value from set, use delete: variable_name.delete(value)
+ */
+
+
+//in sets there are actually no indexes. And in fact, there is no way of getting values out of a set. we cannot call set one by one, but we can use array to call it [...new Set(staff)] like this.
 
 //So in a normal code base, the main use case of sets is actually to remove duplicate values of arrays.
 
-//And so we can now create an array around this basically. And then we can unpack this entire set here using the spread operator, and then these elements will be put into the newly constructed array.
+/**
+ * see line 16
+ * we can create an array around the set
+ * unpack this entire set using spread operato: [...new Set(variable_name)]
+ * then, the element  will be put into the newly constucted array
+ */
+
