3fb141cb805c8d190f32cc37a82e2f217a00bf45
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-22 17:29:00 +0700

    add: string

---
+++ data-structures-operators/string.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const airline = 'TAP Air Indonesia';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[2]);
+console.log(plane[3]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('i'));
+console.log(airline.indexOf('Indonesia'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(-1));
+console.log(airline.slice(1, -1));
+
+
