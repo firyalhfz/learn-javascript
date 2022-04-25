32e132af3ec6f4635e97201c096d1275524e6e88
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-25 17:17:29 +0700

    update: coding challenge #4

--- data-structures-operators/codingchallenge4.js
+++ data-structures-operators/codingchallenge4.js
@@ -27,18 +27,22 @@ GOOD LUCK 😀
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function(){
+document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
-  const rows = text.split('\n')
+  const rows = text.split('\n');
   console.log(rows);
 
-  for(const row of rows){
-    const [first, second] = row.toLowerCase().trim().split('_')
-    console.log(first, second, row);
-    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
-    console.log(output);
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    // console.log(first, second, row);
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
-})
+});
 
 // underscore_case;
 // first_name;
