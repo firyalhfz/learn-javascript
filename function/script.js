eab3866cab71d13e04e3a71e7e6d459547ba165a
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-30 08:27:32 +0700

    add: default parameters

--- function/script.js
+++ function/script.js
@@ -1,1 +1,19 @@
-'use strict'
+'use strict';
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers, price) {
+  numPassengers = numPassengers || 1;
+  price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 808);
+createBooking('LH123', 5);
